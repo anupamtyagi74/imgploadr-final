3f4ed44d8dbea6fe8ce4a327cb27f015c5d8fa4b
crypt0n74 <anupamtyagi74@gmail.com>
2020-05-18 14:36:14 +0530

    updated server.js with mlap URL

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 
 app = config(app);
-
-mongoose.connect('mongodb://imgploadr-finaldb:Anuayu@1@ds215380.mlab.com:15380/heroku_0qmmwqj9',{ useMongoClient: true} );
+var url = process.env.MONGOLAB_URI;
+mongoose.connect(url,{ useMongoClient: true,  useNewUrlParser: true} );
 mongoose.connection.on('open',()=>{
     console.log('Mongoose connected.');
 });
