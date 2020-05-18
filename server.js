6e9caac24a74af289f365a73d4ec551fcc062b55
crypt0n74 <anupamtyagi74@gmail.com>
2020-05-18 16:36:05 +0530

    updated db uri

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     config = require('./server/configure'),
     mongoose = require('mongoose');
-
+var url = process.env.MONGODB_URI || 'mongodb://localhost/imgPloadr';
 var app = express();
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 
 app = config(app);
 
-mongoose.connect('mongodb://localhost/imgPloadr', {
+mongoose.connect(url, {
 	useMongoClient : true
 });
 mongoose.connection.on('open',()=>{
